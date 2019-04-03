34db49178d75c1a595845a4bb1d1837638afeed0
jiangben <zxas5566777@gmail.com>
2019-04-03 21:30:34 +0800

    Initial commit

---
+++ Firebase-Authentication-Demo-master/public/js/firebase.js
@@ -0,0 +1,21 @@
+// Initialize Firebase
+/*
+var config = {
+  apiKey: "AIzaSyD_cHmRKaRmML9SdYfcHq2wPsEY5c4TQks",
+  authDomain: "fir-demo-2a2b1.firebaseapp.com",
+  databaseURL: "https://fir-demo-2a2b1.firebaseio.com",
+  projectId: "fir-demo-2a2b1",
+  storageBucket: "fir-demo-2a2b1.appspot.com",
+  messagingSenderId: "549839046091"
+};
+firebase.initializeApp(config);
+*/
+var config = {
+    apiKey: "AIzaSyBx-mkzBcz5kOd6OgJG3Dv2k6kkz6r0Mho",
+    authDomain: "test-d09d0.firebaseapp.com",
+    databaseURL: "https://test-d09d0.firebaseio.com",
+    projectId: "test-d09d0",
+    storageBucket: "test-d09d0.appspot.com",
+    messagingSenderId: "536394296791"
+  };
+  firebase.initializeApp(config);
